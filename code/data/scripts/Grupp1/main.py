d2bd377f035b4914042b46e44f20d8b15e812462
robwid-9 <robwid-9@student.ltu.se>
2021-03-24 14:13:16 +0100

    Merge remote-tracking branch 'origin/main' into main

--- code/data/scripts/Grupp1/main.py
+++ code/data/scripts/Grupp1/main.py
@@ -1,38 +1,8 @@
-import Grupp1.path_manager as path_manager
-import Grupp1.agent as agent
-import Grupp1.goals as goals
-
-import nmath, demo
-import button_input
-import buildings
-
-y_button = button_input.ButtonInput(demo.IsYDown)
-left_mouse = button_input.ButtonInput(demo.IsRightMouseDown)
-
-castle = buildings.initBlueCastle()
-
-p = castle.Building.position
-a = agent.Agent(nmath.Float2(p.x,p.z));
-
 def NebulaUpdate():
-    global path
+    print("grupp1")
 
-    path_manager.instance.calc_paths(10)
-
-    a.update()
-
-def NebulaDraw(p):
-
-    if left_mouse.pressed():
-        p = demo.RayCastMousePos()
-        a.addGoal(goals.WalkToGoal(p.x,p.z))
-
-<<<<<<< HEAD
 def NebulaDraw():
     print("grupp1 draw")
 
 def HandleMessage(msg):
     pass
-=======
-    a.dbgDraw()
->>>>>>> origin/main
