f9ffd32ba94a9ee1a4ecf99bf1f2349a41da1f9f
Clahul-9 <clahul-9@student.ltu.se>
2021-03-26 12:26:52 +0100

    Entaty manger start

--- code/data/scripts/Grupp2/enums.py
+++ code/data/scripts/Grupp2/enums.py
@@ -17,3 +17,22 @@ class GoalEnum(Enum):
     BUILD_SMITH_GOAL = 8
     BUILD_SMELTER_GOAL = 9
     BUILD_TRAINING_CAMP_GOAL = 10
+
+class UpdateState(Enum):
+    TREES = 0
+    IRON = 1
+    ENEMIES = 2
+
+updateOrder = [
+        UpdateState.IRON,\
+        UpdateState.ENEMIES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES,\
+        UpdateState.TREES]
