0b54b1ef17712c9ba84274d5c549467fca65f4f4
Clahul-9 <clahul-9@student.ltu.se>
2021-04-02 11:04:27 +0200

    hud changes

--- code/data/scripts/Grupp2/hud.py
+++ code/data/scripts/Grupp2/hud.py
@@ -1,15 +1,24 @@
 from Grupp2 import overlord
-import imgui
+import imgui,demo
 
 
 def DrawHUD():
+    builders = 0
+    for a in overlord.overlord.agents:
+        if a.entityHandle.Agent.type == demo.agentType.BUILDER:
+            builders +=1
+
     imgui.Begin("Grupp2 (Red) Inventory", None, 0)
     try:
+        imgui.Text("Resorses: ")
         imgui.Text("Tree: " + str(overlord.overlord.tree))
         imgui.Text("Charcoal: " + str(overlord.overlord.charcoal))
         imgui.Text("Ironbar: " + str(overlord.overlord.ironbar))
         imgui.Text("Ironore: " + str(overlord.overlord.ironore))
         imgui.Text("Sword: " + str(overlord.overlord.sword))
+        imgui.Text("  ")
+        imgui.Text("  ")
+        imgui.Text("Builders: "+ str(builders))
         imgui.End()
     except Exception as e:
         imgui.End()
