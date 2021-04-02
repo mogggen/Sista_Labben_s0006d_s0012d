c6e9608c69857a3e00555b8f9e40e893e5d41562
Clahul-9 <clahul-9@student.ltu.se>
2021-04-02 09:30:38 +0200

    hud changes

--- code/data/scripts/Grupp2/hud.py
+++ code/data/scripts/Grupp2/hud.py
@@ -5,9 +5,16 @@ def DrawHUD():
 
     imgui.Begin("Grupp2 (Red) Inventory", None, 0)
     imgui.Text("Agents: " + str(len(overlord.overlord.agents)))
+    imgui.Text("")
+    imgui.Text("Total workers:" + str(overlord.overlord.nrWorkers))
+    imgui.Text("wood gatherers: " + str(overlord.overlord.nrWoodGatherers))
+    imgui.Text("Iron gatherers" +str(overlord.overlord.nrIronGatherers))
+    imgui.Text("")
+    imgui.Text("builders: " + str(overlord.overlord.nrBuilder))
+    imgui.Text("Soldier: " + str(len(overlord.overlord.soldiers)))
+
     imgui.Text("Buildings: " + str(len(overlord.overlord.buildings)))
     imgui.Text("")
-    imgui.Text("Soldier: " + str(len(overlord.overlord.soldiers)))
     imgui.Text("")
     imgui.Text("Resurser: ")
     imgui.Text("Tree: " + str(overlord.overlord.tree))
